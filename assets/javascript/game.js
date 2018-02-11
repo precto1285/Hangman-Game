0cb968073e38ed6ddc8b7cb2eb050ca23270f37a
Philippe Recto <precto1285@gmal.com>
2018-02-10 18:09:04 -0800

    some progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,9 @@
 var name = ["Anthony", "Philippe", "Dan", "Jan", "Guillermo", "Travell"];
 //Variables of my letters***
 var letter = ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"];
+
+var attemptCount = [10]
+
 //This is where the computer will pick a name from the array
 var gamestart = name[Math.floor(Math.random() * name.length)];
 console.log(gamestart);
@@ -11,16 +14,21 @@ document.onkeyup = function(event){
     var attempt = event.key.toLowerCase();
 }
 
-function myFunction (){
-    var gamestart;
-    var res = gamestart.charArt()
-}
-
-
 //This is where the users choice of letter is evaluated against the name randomly picked by the computer. 
 
-if (attempt === /*letter available in the name selected by the computer*/){
-    // the computer adds the letter into the empty word
+if (var i=0; i < gamestart.length; i++){
+    if(attempt === gamestart[i]){
+        console.log(Gotcha!)
+       return; 
+    }
+    else if (i < gamestart.length; - 1){
+        console.log(Whoops!)
+    }
+    else {
+        console.log("Sorry, Game Over.")
+        attemptCount = attemptCount -1
+    }
+
 }
 else{
 
