805b15cb67e3c904a54084a2252efb95ac8e68d3
Philippe Recto <precto1285@gmal.com>
2018-02-15 14:36:57 -0800

    updated index/css/js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,111 +1,62 @@
-/* //Variables of my classmates names***
-var name = ["Anthony", "Philippe", "Dan", "Jan", "Guillermo", "Travell"];
-//Variables of my letters***
-var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var attemptCount = [10]
+            //global variables/arrays
 
-//This is where the computer will pick a name from the array
-var gamestart = name[Math.floor(Math.random() * name.length)];
-console.log(gamestart);
+            var man = ["m", "a", "n"]
+            var woman = ["w", "o", "m", "a", "n"]
+            var boy = ["b", "o", "y"]
+            var girl = ["g", "i", "r", "l"]
+            var myArray = [man, woman, boy, girl];
+            
+            var underScore = "";
 
-//User controls
-document.onkeyup = function(event){
-    var attempt = event.key.toLowerCase();
-}
-
-//This is where the users choice of letter is evaluated against the name randomly picked by the computer. 
-
-if (var i=0; i < gamestart.length; i++){
-    if(attempt === gamestart[i]){
-        console.log(Gotcha!)
-       return; 
-        }
-    }
-    else if (i < gamestart.length; - 1){
-        console.log(Whoops!)
-    }
-    else {
-        console.log("Sorry, Game Over.")
-        attemptCount = attemptCount -1
-    }
-
-}
-
-*/
-
-
- // taken from https://www.youtube.com/watch?v=dvSKEXmYtMM
-
- var wordblanks = ['anthony', 'travell', 'dan', 'jan', 'guillermo', 'cameron'];
- var wins = 0;
- var loss = 0;
- var wrongLetter = [];
- var guessesLeft = 10;
- var underScores = [];
- var userGuesses = [];
- var ranWord = [];
- var winCounter = 0;
-
-
- //Function
- function startGame(){
-     // picks random word
-     ranWord = wordblanks[Math.floor(Math.random() * wordblanks.length)];
-     console.log('Random Word = ' + ranWord);
-     for(var i = 0; i < ranWord.length; i++)
-     {
-         underScores.push('_');
-     }
-     //Printing underscores to the screen
-     document.getElementById('wordblanks').textContent = underScores.join(" ");
-     console.log(underScores)
-     //reset
-     wrongLetter = [];
-     guessesLeft = 10;
-
-     //HTML
-     document.getElementById('guessesLeft').textContent = guessesLeft;
- }
-
-// function created to alert a winner or loser depending on the guesses of letters inputted    
-function winLose(){
- if(winCounter === ranWord.length){
-     alert('Winner!');
-      
- }
- else if(guessesLeft === 0){
-     alert('You Lose!');
+            var winCount = 0;
+            var loseCount = 0;
+            var wrongGuess = 0;
      
- }
 
-}
+            //Chooses a random word from myArray
 
-//User Guesses
-document.onkeyup = function(event){
- userGuesses = event.key;
- //Checking if the letter exist inside of the word.
- if(ranWord.indexOf(userGuesses) > -1){
-     for(var i = 0; i < ranWord.length; i++){
-         if(ranWord[i] === userGuesses){
-             underScores[i] = userGuesses;
-             console.log(underScores);
-             winCounter++;
-             winLose();
-         }
-     }
- }
- else{
-     wrongLetter.push(userGuesses)
-     guessesLeft--;
-     console.log(wrongLetter);   
-     winLose();
- }
-}        
+            var choice = myArray[Math.floor(Math.random() * myArray.length)];
+                        
+            //replace random word with blanks...
 
- //Main
+            for(var i = 0; i < choice.length; i++){
+                underScore += "_ "
+            }
+            document.getElementById("blanks").innerHTML = underScore;
+         
+            // create a way for the user input letter choices.
 
- startGame();
+            var userInput = prompt("pick a letter from a-z");
+             
 
- //
- console.log(ranWord[0]);
+                    // create a way for picking letters in a string and compare them to user input
+                
+                    for(var i = 0; i < choice.length; i++){
+                    if(userInput == choice.charSet){
+                        alert("Eureka!");
+                        underScore[i] += userInput; 
+                        }
+                    else{
+                        alert("Sorry, Try again");
+                        userInput.push(wrongGuess);                    
+                        }
+                        document.getElementById("wrongGuesses");
+                    }
+                        
+            /* use for loop to have the system go through each letter to see if the 
+            user input matches an index in the random string 
+     
+            // for correct guesses, use for loop to replace blanks with correct letter/s.
+
+
+            //when all letters are filled before or at 10 attempts, user wins!
+            if (){
+                alert("YOU WIN!");
+            }
+            
+            
+            //when number of choices exceeds 10 attempts, user loses
+            else{
+                alert("Sorry, You have been Hung");
+            }*/
