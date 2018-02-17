2ff3b3608877deefd650478c80eaa9ce050e564a
Philippe Recto <precto1285@gmal.com>
2018-02-16 20:44:03 -0800

    tutoring with Jan for Javascript homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,106 +1,241 @@
 
-            //global variables/arrays
+//global variables/arrays
 
-            var man = ["m", "a", "n"]
-            var woman = ["w", "o", "m", "a", "n"]
-            var boy = ["b", "o", "y"]
-            var girl = ["g", "i", "r", "l"]
-            var myArray = [man, woman, boy, girl];
-            
-            var underScore = "";
+var man = ["m", "a", "n"];
+var woman = ["w", "o", "m", "a", "n"];
+var boy = ["b", "o", "y"];
+var girl = ["g", "i", "r", "l"];
+var myArray = [man, woman, boy, girl];
 
-            var numGuess = 10;
+var letterState = [];
 
-            var winCount = 0;
-            var loseCount = 0;
-            var wrongGuess = [];
+var dupeLetters = 0;
+
+var numGuess = 10;
+
+var winCount = 0;
+var loseCount = 0;
+var correctGuess = 0;
+var wrongGuess = [];
+var correctAnswer = "";
+var compChoice = "";
+
+console.log(man);
+console.log(woman);
+console.log(boy);
+console.log(girl);
+console.log(myArray);
+console.log(letterState);
+console.log(dupeLetters);
+console.log(numGuess);
+console.log(winCount);
+console.log(loseCount);
+console.log(wrongGuess);
+console.log(correctGuess);
+console.log(correctAnswer);
+console.log(compChoice);
 
 
-            //Chooses a random word from myArray
+//Chooses a random word from myArray
 
-            var compChoice = myArray[Math.floor(Math.random() * myArray.length)];
-                        
-            //replace random word with blanks...
 
-            for(var i = 0; i < compChoice.length; i++){
-                underScore += "_ "
-                document.getElementById("blanks").innerHTML = underScore;
-            }
+
+//replace random word with blanks...
+
+
+
+alert("HANGMAN!!!");
 //__________________________________________________________________________________________________________________________________________
-                    // create a way for picking letters in a string and compare them to user input
-                    
-                document.onkeyup = function(event) {
-                    var userInput = event.key;
+// create a way for picking letters in a string and compare them to user input
+
+function startGame() {
+    compChoice = myArray[Math.floor(Math.random() * myArray.length)];
+
+    var underScore = "";
+    console.log(underScore);
+    wrongGuess.length = 0;
+    numGuess = 10;
+    letterState.length = 0;
+    
+
+    for (var i = 0; i < compChoice.length; i++) {
+        underScore += "_ ";
+        letterState.length[i] = "unselected";
+    }
+    document.getElementById("blanks").innerHTML = underScore;
+
+}
+
+startGame();
+
+document.onkeyup = function (event) {
+    var userInput = event.key;
+
+    userInput = userInput.toLowerCase();
+    dupeLetters = 0;
+    for (var i = 0; i < compChoice.length; i++) {
+        if (userInput === compChoice[i].key && compChoice[i].value === "correct") {
+            correctAnswer += compChoice[i];
+        }
+        else if (userInput === compChoice[i].key && compChoice[i].value === "unselected") {
+            compChoice[i].value = "correct";
+            dupeLetters++;
+            numGuess--;
+            document.getElementById("guessesLeft").innerHTML = numGuess;
+        }
+        else if (compChoice[i].value === "correct") {
+            correctAnswer += compChoice[i];
+
+        }
+        else {
+            correctAnswer += "_ ";
+        }
+
+    }
+
+    if (dupeLetters > 0) {
+        compChoice[userInput].value = "correct";
+        correctGuesses += dupeLetters;
+
+    }
+
+    document.getElementById("blanks").innerHTML = correctAnswer;
+
+    if (compChoice.length === correctGuess) {
+        alert("You Win!!");
+        startGame();
+    }
+        var isPartofSolution = false;
+    for(var i=0; i<compChoice.length; i++){
+        if(userInput === compChoice[i].key){
+            isPartofSolution = true;
+        }
+        console.log(isPartofSolution);
+    }
+    if (dupeLetters === 0 && !isPartofSolution) {
+        wrongGuess.push(userInput);
+        numGuess--;
+        document.getElementById("guessesLeft").innerHTML = numGuess;
+        var wrongLetterDisplay = "";
+        console.log(wrongLetterDisplay);
+        for (var i = 0; i < wrongGuess.length; i++) {
+            wrongLetterDisplay += wrongGuess[i] + " ";
+        }
+        document.getElementById("wrongGuess").innerHTML = wrongLetterDisplay;
+    }
+
+    if (numGuess === 0) {
+        loseCount++;
+        alert("You Suck!");
+        document.getElementById("loseCount").innerHTML = loseCount;
+        startGame();
+    }
+}
+
+
+
+
+/*               
                         if(compChoice === man){
+                        document.onkeyup = function(event) {
+                               var userInput = event.key;
                             if (userInput === man[0]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[0];    
+                                    var ansM = userInput += underScore[0];
+                                document.getElementById("blanks").innerHTML = ansM;    
                             }
                             if (userInput === man[1]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[1];   
+                                    var ansA = userInput += underScore[1];
+                                document.getElementById("blanks").innerHTML = ansA;   
                             }
                             if (userInput === man[2]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[2];    
+                                    var ansN =userInput += underScore[2];
+                                document.getElementById("blanks").innerHTML = ansN;    
                             }
                             else{
                                 console.log("Sorry, Try again");
-                                document.getElementById("wrongGuess").innerHTML = wrongGuess;                    
+                                document.getElementById("wrongGuess").innerHTML = wrongGuess + userInput;                    
                                 }       
                         }
+                        }
+
                         if(compChoice === woman){
+                        document.onkeyup = function(event) {
+                                var userInput = event.key;    
                             if (userInput === woman[0]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[0];   
+                                    var ansW = userInput += underScore[0];
+                                document.getElementById("blanks").innerHTML = ansW;   
                             }
                             if (userInput === woman[1]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[1];   
+                                    var ansO = userInput += underScore[1];
+                                document.getElementById("blanks").innerHTML = ansO;   
                             }
                             if (userInput === woman[2]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[2];    
+                                    var ansM = userInput += underScore[2]
+                                document.getElementById("blanks").innerHTML = ansM;    
                             }
                             if (userInput == woman[3]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[3];    
+                                    var ansA =userInput += underScore[3]
+                                document.getElementById("blanks").innerHTML = ansA;    
                             }
                             if (userInput === woman[4]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[4];   
+                                    var ansN = userInput += underScore[4]
+                                document.getElementById("blanks").innerHTML = ansN;   
                             }else{
                                 console.log("Sorry, Try again");
-                                document.getElementById("wrongGuess").innerHTML = wrongGuess;                    
+                                document.getElementById("wrongGuess").innerHTML = wrongGuess + userInput;                    
                                 } 
                         }   
+                        }                        
+                        
                         if(compChoice === boy){
+                        document.onkeyup = function(event) {
+                                var userInput = event.key;
                             if (userInput === boy[0]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[0];   
+                                    var ansB = userInput += underScore[0]
+                                document.getElementById("blanks").innerHTML = ansB;   
                             }
                             if (userInput === boy[1]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[1];   
+                                    var ansO = userInput += underScore[1]
+                                document.getElementById("blanks").innerHTML = ansO;   
                             }
                             if (userInput === boy[2]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[2];    
+                                    var ansY = userInput += underScore[2]
+                                document.getElementById("blanks").innerHTML = ansY;    
                                 }
                             else{
                                 console.log("Sorry, Try again");
-                                document.getElementById("wrongGuess").innerHTML = wrongGuess;                    
+                                document.getElementById("wrongGuess").innerHTML = wrongGuess + userInput;                    
                                 }
                         }
-                        if (compChoice === girl){                     
-                            if (userInput === girl[0]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[0];   
-                            }
-                            if (userInput === girl[1]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[1];   
-                            }
-                            if (userInput === girl[2]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[2];    
-                            }
-                            if (userInput === girl[3]){
-                                document.getElementById("blanks").innerHTML = userInput += underScore[3];    
-                            }
-                            else{
-                                console.log("Sorry, Try again");
-                                document.getElementById("wrongGuess").innerHTML = wrongGuess;                    
-                                }                           
                         }
                         
-                }            
+                        if (compChoice === girl){
+                        document.onkeyup = function(event) {
+                                var userInput = event.key;                     
+                            if (userInput === girl[0]){
+                                    var ansG = userInput += underScore[0]
+                                document.getElementById("blanks").innerHTML = ansG;   
+                            }
+                            if (userInput === girl[1]){
+                                    var ansI = userInput += underScore[1]
+                                document.getElementById("blanks").innerHTML = ansI;   
+                            }
+                            if (userInput === girl[2]){
+                                    var ansR = userInput += underScore[2]
+                                document.getElementById("blanks").innerHTML = ansR;    
+                            }
+                            if (userInput === girl[3]){
+                                    var ansL = userInput += underScore[3]
+                                document.getElementById("blanks").innerHTML = ansL;    
+                            }
+                            else{
+                                console.log("Sorry, Try again");
+                                document.getElementById("wrongGuess").innerHTML = wrongGuess + userInput;                    
+                                }                           
+                        }
+                        }
+                        
+                            
                         
 
 /*
@@ -114,4 +249,4 @@
                 loseCount + 1;
             }
             
-*/            
+*/
